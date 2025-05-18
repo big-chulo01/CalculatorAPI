4556cc72a38a4247495ba9b430fd3adb1ac5417f
SAFE <SAFE@DESKTOP-D6906Q6>
2025-05-18 15:32:41 +0100

    Add project files.

---
+++ CalculatorAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("Add/{num1}/{num2}")]
+        public decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpGet("Subtract/{num1}/{num2}")]
+        public decimal Subtract(decimal num1, decimal num2)
+        {
+            return num1 - num2;
+        }
+
+        [HttpGet("Multiply/{num1}/{num2}")]
+        public decimal Multiply(decimal num1, decimal num2)
+        {
+            return num1 * num2;
+        }
+
+        [HttpGet("Divide/{num1}/{num2}")]
+        public decimal Divide(decimal num1, decimal num2)
+        {
+            return num1 / num2;
+        }
+
+        [HttpGet("Modulo/{num1}/{num2}")]
+        public decimal Modulo(decimal num1, decimal num2)
+        {
+            return num1 % num2;
+        }
+    }
+}
